4aa312b886573db0ba846ee76e8bd2cd8c36bf5d
riteshsandilya2024 <kr1998ritesh@gmail.com>
2024-11-10 22:44:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,1 @@
-/**
- * Entry point of application, where App is rendered within the div with the id of "app"
- */
 
-import React from "react";
-import { render } from "react-dom";
-
-import App from "./App";
-
-render(<App/>, document.getElementById("app"));
